9e30ddd10ec6028543faed21171303eb9df9f039
Ciaran <ciaran.gruber@my.jcu.edu.au>
2018-08-11 21:54:03 +1000

    Refactor password program to use functions

--- prac_03/password_check.py
+++ prac_03/password_check.py
@@ -7,14 +7,26 @@ MIN_LENGTH = 5
 
 def main():
     """Check password"""
-    password = ''
+    password = get_password()
+    print_password(password)
+
+
+def print_password(password):
+    """Print password"""
+    for x in range(len(password)):
+        print('*', end='')
+    print()
+
+
+def get_password():
+    """Get the password"""
+    password = input('Please type in your password')
     while not is_min_length(password, MIN_LENGTH):
+        print('Please type in an appropriate password')
+        print()
         password = input('Please type in your password')
-        if is_min_length(password, MIN_LENGTH):
-            for x in range(len(password)):
-                print('*', end='')
-        else:
-            print('Please type in an appropriate password')
+        print()
+    return password
 
 
 def is_min_length(text, min_length):
