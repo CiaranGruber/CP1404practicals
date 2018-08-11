1108bf5ffb730640da8afcb340b6af8783b9ecef
Ciaran <ciaran.gruber@my.jcu.edu.au>
2018-08-11 20:57:54 +1000

    Remove irrelevant TODO

--- prac_01/temperatures.py
+++ prac_01/temperatures.py
@@ -14,7 +14,6 @@ while choice != "Q":
         fahrenheit = celsius * 9.0 / 5 + 32
         print("Result: {:.2f} F".format(fahrenheit))
     elif choice == "F":
-        # TODO: Write this section to convert F to C and display the result
         fahrenheit = float(input("Fahreinheit: "))
         celsius = (fahrenheit - 32) * 5 / 9
         print("Result: {:.2f} F".format(fahrenheit))
