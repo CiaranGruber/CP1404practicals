ff6662d8b9ea05e01744ec2d8f90120db161a3c9
CiaranGruber <ciaran.gruber@my.jcu.edu.au>
2018-08-14 12:49:19 +1000

    Complete security_checker task

---
+++ prac_04/security_checker.py
@@ -0,0 +1,11 @@
+def main():
+    usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface',
+                 'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer',
+                 'bob']
+    if input('Username: ') in usernames:
+        print('Access granted')
+    else:
+        print('Access denied')
+
+
+main()
