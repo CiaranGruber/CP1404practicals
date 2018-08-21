0258f7601ebaf28ceaedf186f88c4e3525948246
CiaranGruber <ciaran.gruber@my.jcu.edu.au>
2018-08-21 13:59:57 +1000

    Reformat electricity_bill_estimator extension and add docstring

--- prac_05/extension_electricity_bill_estimator.py
+++ prac_05/extension_electricity_bill_estimator.py
@@ -1,15 +1,23 @@
-TARIFF_11 = 0.244618
-TARIFF_31 = 0.136928
+"""
+Electricity bill estimator that estimates bill according to user input
+
+Electricity Bill Estimator. Created by Ciaran Gruber - 21/08/18
+"""
+
 TARIFFS = {'11': 0.244618, '31': 0.136928}
 
-print("Electricity bill estimator 2.0")
 
-tariff = input("Which tariff? ")
-while tariff not in TARIFFS:
+def main():
+    """Electricity bill estimator"""
+    print("Electricity bill estimator 2.0")
     tariff = input("Which tariff? ")
-dollars_per_kWh = TARIFFS[tariff]
+    while tariff not in TARIFFS:
+        tariff = input("Which tariff? ")
+    dollars_per_kWh = TARIFFS[tariff]
+    kWh_daily_use = float(input("Enter daily use in kWh: "))
+    billing_days = int(input("Enter number of billing days: "))
+    bill = round(dollars_per_kWh * kWh_daily_use * billing_days, 2)
+    print("Estimated bill: $" + str(bill))
 
-kWh_daily_use = float(input("Enter daily use in kWh: "))
-billing_days = int(input("Enter number of billing days: "))
-bill = round(dollars_per_kWh * kWh_daily_use * billing_days, 2)
-print("Estimated bill: $" + str(bill))
+
+main()
