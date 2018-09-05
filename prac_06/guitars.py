17d407f81cd7d8dc6984973c40f0e5196f6d841a
CiaranGruber <ciaran.gruber@my.jcu.edu.au>
2018-09-05 22:11:09 +1000

    Refeactor guitar display table and fix small error

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -13,8 +13,8 @@ def main():
     print()
     name = input('Name: ')
     while name != '':
-        year = get_number('Year: ', 'Year must be a number', 'Year must be above 0', 0, 2018, 'int')
-        cost = get_number('Cost: ', 'Year must be a number', 'Year must be above 0', 0)
+        year = get_number('Year: ', 'Year must be a number', 'Year must be above 0 and below 2018', 0, 2018, 'int')
+        cost = get_number('Cost: ', 'Cost must be a number', 'Cost must be above 0', 0)
         guitars.append(Guitar(name, year, cost))
         print()
         name = input('Name: ')
@@ -23,9 +23,12 @@ def main():
     longest_word = get_largest_name(guitars)
     longest_cost = get_largest_cost(guitars)
     for i, guitar in enumerate(guitars):
-        print("Guitar {0}: {self.name:>{1}} ({self.year}), worth ${self.cost:{2},.2f}{3}"
-              .format(i + 1, longest_word, longest_cost,
-                      ('(vintage)', '')[0 if guitar.get_age() > 50 else 1], self=guitar))
+        print("Guitar {0}: {1:>{2}} ({3}), worth ${4:{5}.2f}".format(i + 1, guitar.name, longest_word,
+                                                                     guitar.year, guitar.cost, longest_cost), end='')
+        if guitar.is_vintage():
+            print(' (vintage)')
+        else:
+            print()
 
 
 def get_number(prompt, error_prompt, limit_prompt, min_num=0 - float('inf'), max_num=float('inf'), valid_type='either'):
