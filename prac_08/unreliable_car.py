a7c849de4aa86c8e01390153ef7d17c5f23ce10d
CiaranGruber <ciaran.gruber@my.jcu.edu.au>
2018-09-22 15:40:08 +0100

    Complete UnreliableCar task

---
+++ prac_08/unreliable_car.py
@@ -0,0 +1,22 @@
+"""
+Create the UnreliableCar class
+
+Unreliable Car Class. Created by Ciaran Gruber - 22/09/18
+"""
+
+import random
+from prac_08.car import Car
+
+
+class UnreliableCar(Car):
+    """Represent an UnreliableCar object"""
+
+    def __init__(self, name, fuel, reliability):
+        """Initialise an UnreliableCar instance"""
+        super().__init__(name, fuel)
+        self.reliability = reliability
+
+    def drive(self, distance):
+        """Have a chance of driving the UnreliableCar depending on its reliability"""
+        if random.randint(0, 100) < self.reliability:
+            return super().drive(100)
