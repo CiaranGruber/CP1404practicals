b58bfdbe81905122e7ef52803f66b60f6a5216dc
CiaranGruber <ciaran.gruber@my.jcu.edu.au>
2018-09-22 15:33:27 +0100

    Complete taxi_test task

---
+++ prac_08/taxi_test.py
@@ -0,0 +1,21 @@
+"""
+Test the Taxi Class
+
+Taxi Class Test. Created by Ciaran Gruber - 22/09/18
+"""
+
+from prac_08.taxi import Taxi
+
+
+def main():
+    new_taxi = Taxi('Prius 1', 100, 1.23)
+    new_taxi.drive(40)
+    print(new_taxi)
+    print('Current Fare:', new_taxi.get_fare())
+    new_taxi.start_fare()
+    new_taxi.drive(100)
+    print(new_taxi)
+    print('Current Fare:', new_taxi.get_fare())
+
+
+main()
