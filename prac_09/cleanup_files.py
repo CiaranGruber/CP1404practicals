8f0704ba83eb2f5b9cd666ba56643d8bfc0ff42f
Ciaran <ciaran.gruber@my.jcu.edu.au>
2018-10-16 09:48:49 +0100

    Fix get_fixed_filename function and add LYRICS_FOLDER constant

--- prac_09/cleanup_files.py
+++ prac_09/cleanup_files.py
@@ -4,26 +4,29 @@ Demos of various os module examples
 """
 import os
 
+LYRICS_FOLDER = 'Lyrics'
+
 
 def get_fixed_filename(filename):
     """Return a 'fixed' version of filename."""
-    filename = filename.replace('.TXT', '.txt')
     new_name = ""
     for i, letter in enumerate(filename):
-        if letter.isupper() and i != 0:
+        if letter == '.':
+            new_name += '.' + filename.split('.')[1]
+            break
+        if letter.isupper() and i != 0 and new_name[-1] not in ['(', ')', '_']:
             new_name += '_'
         if letter == ' ':
             new_name += '_'
         else:
             new_name += letter
-    new_name = '_'.join([name.title() for name in new_name.split('_')])
-    new_name = new_name.replace('.Txt', '.txt')
+    new_name = new_name.title().replace('.Txt', '.txt')
     return new_name
 
 
 def main():
     """Process all subdirectories using os.walk()."""
-    os.chdir('Lyrics')
+    os.chdir(LYRICS_FOLDER)
     for directory_name, subdirectories, filenames in os.walk('.'):
         print("Directory:", directory_name)
         print("\tcontains subdirectories:", subdirectories)
